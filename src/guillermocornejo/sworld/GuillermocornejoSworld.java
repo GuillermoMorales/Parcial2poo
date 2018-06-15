58759959e3e34f1f07f2a9ca41fee710eb51df27
Guillermo <00011417@uca.edu.sv>
2018-06-14 21:43:04 -0600

    Continuando menu

--- src/guillermocornejo/sworld/GuillermocornejoSworld.java
+++ src/guillermocornejo/sworld/GuillermocornejoSworld.java
@@ -5,6 +5,15 @@
  */
 package guillermocornejo.sworld;
 
+import Humanos.Humano;
+import Demonios.Demonio;
+import Demonios.NovenoCirculo;
+import Dioses.Dios;
+import Dioses.MonteOlimpo;
+import Humanos.HeadQuarters;
+import java.util.Scanner;
+import Other.usuario;
+import java.util.ArrayList;
 /**
  *
  * @author Guille
@@ -15,7 +24,131 @@ public class GuillermocornejoSworld {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        // TODO code application logic here
+       
+       //Humanos
+        HeadQuarters hq=new HeadQuarters();
+        HeadQuarters hq2=new HeadQuarters();
+        ArrayList<Humano> moro=new ArrayList<Humano>();
+        ArrayList<Humano> moro2=new ArrayList<Humano>();
+        ArrayList<Humano> mopla=new ArrayList<Humano>();
+        ArrayList<Humano> mopla2=new ArrayList<Humano>();
+        ArrayList<Humano> moco=new ArrayList<Humano>();
+        ArrayList<Humano> moco2=new ArrayList<Humano>();
+        //Demonios
+        NovenoCirculo nc = new NovenoCirculo();
+        NovenoCirculo nc2 = new NovenoCirculo();
+        ArrayList<Demonio> alma=new ArrayList<Demonio>();
+        ArrayList<Demonio> alma2=new ArrayList<Demonio>();
+        ArrayList<Demonio> pecado=new ArrayList<Demonio>();
+        ArrayList<Demonio> pecado2=new ArrayList<Demonio>();
+        ArrayList<Demonio> monjudas=new ArrayList<Demonio>();
+        ArrayList<Demonio> monjudas2=new ArrayList<Demonio>();
+        //Dioses
+        MonteOlimpo mo = new MonteOlimpo();
+        MonteOlimpo mo2 = new MonteOlimpo();
+        ArrayList<Dios> plan=new ArrayList<Dios>();
+        ArrayList<Dios> plan2=new ArrayList<Dios>();
+        ArrayList<Dios> moneda=new ArrayList<Dios>();
+        ArrayList<Dios> moneda2=new ArrayList<Dios>();
+        ArrayList<Dios> mena=new ArrayList<Dios>();
+        ArrayList<Dios> mena2=new ArrayList<Dios>();
+        //Variables
+        Scanner entrada = new Scanner(System.in);
+        String num;
+        boolean flag = false;
+        ///////////////////////////////////////////////////////////////////////
+        System.out.println("--------------------BIENVENIDO AL JUEGO--------------------");
+        System.out.println("Razas disponibles:\n"
+                + "1)Demonios\n"
+                + "2)Dioses\n"
+                + "3)Humanos\n");
+        do
+        {
+            System.out.println("Jugador 1 seleccione su raza: ");
+            num= entrada.nextLine();
+            if (num.matches("[1-3]")) 
+            {
+                flag=true;
+                int numero = Integer.parseInt(num);
+                switch (numero) {
+                    case 1:
+                        {
+                            System.out.println("Has seleccionado la raza Demonios");
+                            usuario u1=new usuario( 1 , num, nc,alma,pecado,monjudas);
+                            break;
+                        }
+                    case 2:
+                        {
+                            System.out.println("Has seleccionado la raza Humanos");
+                            usuario u1 = new usuario(1 , num, hq,moro,mopla,moco);
+                            break;
+                        }
+                    case 3:
+                        {
+                            System.out.println("Has seleccionado la raza Dioses");
+                            usuario u1 = new usuario(1 , num, mo,plan,moneda,mena);
+                            break;
+                        }
+                    default:
+                        break;
+                }
+                
+            } 
+            else 
+            {
+                flag=false;
+                System.out.println("Dato inválido, vuelva a intentar");
+                System.out.println("Razas disponibles:\n"
+                + "1)Demonios\n"
+                + "2)Dioses\n"
+                + "3)Humanos\n");
+            }
+        }while(flag==false);
+        
+        //////////////////////////////////////////////////////////////////////
+        //Jugador 2
+        do
+        {
+            System.out.println("Jugador 2 seleccione su raza: ");
+            num= entrada.nextLine();
+            if (num.matches("[1-3]")) 
+            {
+                flag=true;
+                int numero = Integer.parseInt(num);
+                switch (numero) {
+                    case 1:
+                        {
+                            System.out.println("Has seleccionado la raza Demonios");
+                            usuario u2=new usuario( 2 , num, nc2,alma2,pecado2,monjudas2);
+                            break;
+                        }
+                    case 2:
+                        {
+                            System.out.println("Has seleccionado la raza Humanos");
+                            usuario u2 = new usuario(2 , num, hq2,moro2,mopla2,moco2);
+                            break;
+                        }
+                    case 3:
+                        {
+                            System.out.println("Has seleccionado la raza Dioses");
+                            usuario u2 = new usuario( 2, num, mo2,plan2,moneda2,mena2);
+                            break;
+                        }
+                    default:
+                        break;
+                }
+                
+            } 
+            else 
+            {
+                flag=false;
+                System.out.println("Dato inválido, vuelva a intentar");
+                System.out.println("Razas disponibles:\n"
+                + "1)Demonios\n"
+                + "2)Dioses\n"
+                + "3)Humanos\n");
+            }
+        }while(flag==false);
+         
     }
-    
 }
